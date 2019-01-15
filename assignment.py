8a1f1a54c9e451e9395d10c5dc2bed238488a526
russian-sadness <a.nosanova@students.uu.nl>
2019-01-15 15:54:31 +0100

    Part 2 was added

--- assignment.py
+++ assignment.py
@@ -29,11 +29,24 @@ final_list = [0]*12
 for line in Seattle:
     month = int(line['month']) 
     final_list[month-1] = line['value'] +  final_list[month-1]
-print(final_list)
+#print(final_list)
 
 import json
 with open('Seattle_months.json', 'w') as file: 
     json.dump(final_list, file , indent=4 , sort_keys = True ) 
+
+# Part 2
+#Calculate the sum of the precipitation over the whole year., we use the final_list which has the precipations per month 
+total_precipation = int(sum(final_list))
+print('The total precipation is: ' + str(total_precipation))
+
+# Calculate the relative precipitation per month (percentage compared to the precipitation over the whole year)
+# We have to devide each value in the final_list by the total and multiply by 100 (and we loop it around the final_list)
+relative_presipation = [100*i/total_precipation for i in final_list]
+print(relative_presipation)
+
+
+
 
 
 
